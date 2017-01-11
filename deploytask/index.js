4699ec54d834e456ff90bcea8145b4f2149f6994
Levi Fuller <Levi Fuller>
2017-01-10 21:55:32 -0700

    Might be working.

---
+++ deploytask/index.js
@@ -0,0 +1,44 @@
+var path = require('path'),
+    task = require('vsts-task-lib/task'),
+    shelljs = require('shelljs'),
+    fs = require('fs'),
+    http = require('https');
+//process.env[""]
+
+// var myPath = task.getInput('samplepathinput');
+
+
+task._writeLine('File Byte Count: ' + zipFile.byteLength);
+task._writeLine('Deploying ' + fileName);
+
+var username = "lfuller941@production";
+var password = task.getVariable('password');
+var applicationName = task.getInput('applicationname', true);
+//var fileName = task.getInput('filename', true);
+var folder = task.getPathInput('filepath', true).replace(/\"/g, "");
+var fileName = 'mstest-1.0.0-SNAPSHOT.zip';
+var fullFilePath = path.join(folder, fileName);
+
+
+var zipFile = fs.readFileSync(fullFilePath);
+
+var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+var header = {'Authorization': auth}
+
+
+var req = http.request({
+    method: "POST",
+    host: 'anypoint.mulesoft.com',
+    path: '/cloudhub/api/applications/'+ applicationName +'/deploy',
+    headers: header
+}, function(e){
+    task._writeLine(e.statusCode + ' ' + e.statusMessage + ' - Application Deployed Successfully')
+}).on('error', function(e){
+    task._writeLine(e.name + ' ERROR: ' + e.message)
+});
+
+req.write(zipFile);
+req.end();
+
+
+    
